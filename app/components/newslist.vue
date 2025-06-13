b4d4eca92730597f2621f3e97d7e893bd780791f
纪中庆 <imjzq@qq.com>
2025-06-13 14:44:52 +0800

    diff

--- app/components/newslist.vue
+++ app/components/newslist.vue
@@ -1,16 +1,12 @@
 <script setup lang='ts'>
 
-const { data: list } = await useRequest(get('/business/information/getNewsList'),{  initialData: [{
-    infoId: '',
-    title: '',
-    publishTime: '2025-06-05 16:15:34',
-    content: '<p></p>',
-  }]}) 
+const { data: list } = await useRequest(get('/business/information/getNewsList')) // 十秒后切换过来，不请求
+// const { data: list } = await useAsyncData(async () => await get('/business/information/getNewsList')) // 可以
 </script>
 
 <template>
   <div>
-    <div class="" v-if="list.length">请求数据：{{ JSON.stringify(list) }}</div>
+    <div class="" v-if="list?.length">请求数据：{{ JSON.stringify(list) }}</div>
     <div class="" v-else>没有请求</div>
   </div>
 </template>
